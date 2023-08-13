e0f8beffe75e772140b6edeb0883773ddd4fe3b9
Alanwoke Martins <alanwokemartins@gmail.com>
2023-08-13 12:38:50 -0700

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	printf("Size of char: 1  byte(s)", sizeof(char));
+	printf("Size of char: 1 byte(s)", sizeof(char));
 	printf("Size of int: 4 byte(s)", sizeof(int));
 	printf("Size of long int: 8 byte(s)", sizeof(long int));
 	printf("Size of long long int: 8 byte(s)", sizeof(long long int));
