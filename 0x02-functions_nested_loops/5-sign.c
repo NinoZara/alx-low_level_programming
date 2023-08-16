cead508851477f35615db11312ca5578963b90bb
Alanwoke Martins <alanwokemartins@gmail.com>
2023-08-16 11:53:04 -0700

    5 sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: Always 1 or 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{ _putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{ 
+		_putchar('0');
+		return (0);
+	}
+	else 
+	{
+		_putchar('-');
+			return (-1);
+	}
+}
