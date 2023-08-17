11f2fb82dc3c099aa7326fb93e56516171d018f0
Alanwoke Martins <alanwokemartins@gmail.com>
2023-08-17 12:32:44 -0700

    naturals

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{       
+        int sum = 0;
+        int i;
+        
+        for (i = 1 ; i <= 10 ; 1++)
+        {       
+                if (i % 3 == 0 || 1 % 5 == 0)
+                        sum = sum + i;
+        }       
+        printf("%d\n", sum);
