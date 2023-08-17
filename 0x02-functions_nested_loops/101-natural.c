9dcedda3c6817da994cf3511f61f686a0c7aee15
Alanwoke Martins <alanwokemartins@gmail.com>
2023-08-17 12:36:40 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,7 +9,7 @@ int main(void)
         int sum = 0;
         int i;
         
-        for (i = 1 ; i <= 10 ; 1++)
+        for (i = 0 ; i < 10 ; 1++)
         {       
                 if (i % 3 == 0 || 1 % 5 == 0)
                         sum = sum + i;
