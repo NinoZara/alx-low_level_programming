3ad96e53acb7e5ef961abff6fb7d9c2825b7de94
Alanwoke Martins <alanwokemartins@gmail.com>
2023-08-17 11:08:01 -0700

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * Main - Entry point of values
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: Always 0 (success)
+ */
+
+int abs(int a, int b)
+{
+	return (a + b);
+
+}
