78b40a7b806bbded5b436e453d60791941d9b452
Alanwoke Martins <alanwokemartins@gmail.com>
2023-08-17 12:43:17 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,10 +9,11 @@ int main(void)
         int sum = 0;
         int i;
         
-        for (i = 0 ; i < 10 ; 1++)
+        for (i = 0 ; i < 1024 ; i++)
         {       
-                if (i % 3 == 0 || 1 % 5 == 0)
+                if (i % 3 == 0 || i % 5 == 0)
                         sum = sum + i;
         }       
         printf("%d\n", sum);
+	return (0);
 }
