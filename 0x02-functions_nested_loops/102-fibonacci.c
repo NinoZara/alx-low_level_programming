c00ec6471d5cf5bb2030dd2f04dbb4a4edbbcb5a
Alanwoke Martins <alanwokemartins@gmail.com>
2023-08-17 13:07:05 -0700

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,15 +12,13 @@ int main(void)
 	int next;
 	int i;
 
-	printf("%lu, ", term1);
+	printf("%d, ", term1);
 	for (i = 1; i < 50 ; i++)
 	{
-		printf("%lu", term2);
+		printf("%d", term2);
 		next = term1 + term2;
 		term1 = term2;
 		term2 = next;
-		if (i != 49)
-			printf(", ");
 	}
 	printf("\n");
 	return (0);
