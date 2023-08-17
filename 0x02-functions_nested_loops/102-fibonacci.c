478fea8b53fc8a04f2882c446b96627f46582b6d
Alanwoke Martins <alanwokemartins@gmail.com>
2023-08-17 12:57:05 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ * Return: 0
+ */
+
+int main(void)
+{
+	int term1 = 1;
+	int term2 = 2;
+	int next;
+	int i;
+
+	printf("%d, ", term1);
+	for (i = 1; i < 50 ; i++)
+	{
+		printf("%d", term2);
+		next = term1 + term2;
+		term1 = term2
+	}
+	printf("\n");
+	return (0);
+}
